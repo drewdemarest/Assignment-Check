24c149aa7de402c05f7b9f7aadd3d0f1bc4e46c0
Unknown <drewdemarest@gmail.com>
2018-02-26 15:33:17 -0800

    DEVEL: Added resource QRC for icons
    App now has icon

--- main.cpp
+++ main.cpp
@@ -7,10 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowIcon(QIcon(":/gmac.png"));
     w.show();
 
-    //QString(QApplication::applicationDirPath()+ "/client.json"
-    //OAuth2 derp(QString(QApplication::applicationDirPath() + "/oauth2Settings.db"), QString(QApplication::applicationDirPath()+ "/client.json"), "https://www.googleapis.com/auth/spreadsheets.readonly");
-    //qDebug() << derp.get("https://sheets.googleapis.com/v4/spreadsheets/1KA7c9bbG2p4f8SFe5ibbkIycwt0wukRe2_xpTB3SI6A/values/Route Start Times");
     return a.exec();
 }
